64d803b17ca2c5411900125b7b8d5ec1f65fc4ba
igd93 <102153262+igd93@users.noreply.github.com>
2025-03-20 19:03:55 +0100

    - Added aditional alembic configs
    - Added base script for alembic migrations

---
+++ backend/app/models/file.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+class File(Base):
+    __tablename__ = "files"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    uploader_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    filename = Column(String(255), nullable=False)
+    storage_key = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    
+    # Define relationships
+    uploader = relationship("User", back_populates="files") 
